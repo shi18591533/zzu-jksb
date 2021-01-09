2879ab9f89ff9ea058dbcb9178a9e31775598621
shi18591533 <77183145+shi18591533@users.noreply.github.com>
2021-01-09 21:18:03 +0800

    Update jksb.py

--- jksb.py
+++ jksb.py
@@ -66,7 +66,7 @@ class Log:
                 if '已经填报' in init_text:
                 # if '1' in init_text:
                     #self.fp.writelines(u"{} --> 打卡成功 -->已填报过了 无需重复填写  ^_^ \n ".format(time.strftime("%Y-%m-%d  %H : %M: %S", time.localtime())))
-                    print(u"{}  已完成上报啦 ，无需重复啦 ^_^ ".format(time.strftime("%Y-%m-%d  %H : %M: %S", time.localtime(time.time() + 8*3600))))
+                    print(u"{}  已完成上报啦 ，无需重复啦哈 ^_^ ".format(time.strftime("%Y-%m-%d  %H : %M: %S", time.localtime(time.time() + 8*3600))))
                     time.sleep(5)
                     sys.exit()
                 else:
